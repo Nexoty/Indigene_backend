0b63de0045247ead1a553c84ffc1ee719f912f7f
Nexoty <111254988+Nexoty@users.noreply.github.com>
2025-08-13 17:05:08 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app =Flask(__name__)
+CORS(app)
+
+
+db = mysql.connector.connect(
+    host='82.197.82.14',      # ou '127.0.0.1'
+    port=3306,             # port par défaut MySQL (optionnel si port 3306)
+    user='u119316410_nexoty',
+    password='7|lQ*G&ZG@w3',
+    database='u119316410_indigene'
+)
+
+
+cursor = db.cursor(dictionary=True)
+#Inserer
+@app.route('/inserer',methods=['POST'])
+def creer_alerte():
+    data =request.json
+    confirmation=1
+    sql="INSERT INTO alerte (id_utilisateur,type,latitude,longitude,confirmation,image)VALUES(%s,%s,%s,%s,%s,%s)"
+    values=(data['uid'],data['type'],data['latitude'],data['longitude'],confirmation,data['image'])
+    cursor.execute(sql,values)
+    db.commit()
+    return jsonify ({"success":True,'message':'creation alerte','id':cursor.lastrowid})
+
+#Recuperer
+@app.route('/recuperer',methods=['GET'])
+def recuperer_alerte():
+    cursor.execute("SELECT * FROM alerte")
+    resultats=cursor.fetchall()
+    return jsonify(resultats)
+
+#Mise a jour
+@app.route('/update',methods=['POST'])
+def mise_a_jour_alerte():
+    data =request.json
+    confirmation=1
+    sql="UPDATE alerte SET confirmation=%s WHERE id=%s"
+    cursor.execute(sql,(confirmation,data['id']))
+    return  jsonify({'success':True,'message':"Alerte mise a jour"})
+
+@app.route('/effacer',methods=['POST'])
+def effacer_alerte():
+    data=request.json
+    sql="DELETE FROM alerte WHERE id=%s"
+    cursor.execute(sql,data['id'])
+    return jsonify({'success':True,'message':"Le danger est eloigne"})
+
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
